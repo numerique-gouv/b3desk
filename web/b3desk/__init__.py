db3ea07239177f6f3f0443b93a206f57576a1cbd
Loan Robert <loan.rbrt@gmail.com>
2024-04-22 16:13:07 +0200

    chore: bump to 1.2.2

--- web/b3desk/__init__.py
+++ web/b3desk/__init__.py
@@ -29,7 +29,7 @@ from b3desk.utils import is_rie
 from .utils import enum_converter
 from .utils import model_converter
 
-__version__ = "1.2.2dev"
+__version__ = "1.2.2"
 
 LANGUAGES = ["en", "fr"]
 
