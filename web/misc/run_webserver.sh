fa1646f8484e6d25f3f5778d9bf8832be272eb63
Loan Robert <loan.rbrt@gmail.com>
2024-04-25 12:33:04 +0200

    Change gunicorn logging level to info for production

--- web/misc/run_webserver.sh
+++ web/misc/run_webserver.sh
@@ -3,4 +3,4 @@
 # DB Migration
 flask db upgrade &>> /var/log/flask-migrate.log
 
-gunicorn --chdir /opt/bbb-visio --bind 0.0.0.0:5000 --log-level warning --access-logfile /var/log/gunicorn-access.log --error-logfile /var/log/gunicorn-error.log wsgi:app
+gunicorn --chdir /opt/bbb-visio --bind 0.0.0.0:5000 --log-level info --access-logfile /var/log/gunicorn-access.log --error-logfile /var/log/gunicorn-error.log wsgi:app
