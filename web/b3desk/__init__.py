26d28c805eb9142c1a9144d262e7867bd95cb73b
Klorydryk <acyd_klorydryk@hotmail.com>
2024-07-22 16:48:37 +0200

    Update main to 1.2.14xdev version

--- web/b3desk/__init__.py
+++ web/b3desk/__init__.py
@@ -29,7 +29,7 @@ from b3desk.utils import is_rie
 from .utils import enum_converter
 from .utils import model_converter
 
-__version__ = "1.2.13dev"
+__version__ = "1.2.14dev"
 
 LANGUAGES = ["en", "fr"]
 
