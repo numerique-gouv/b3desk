c9bde92e65fb84c61db0afbbc96c437e0560c87b
Loan Robert <loan.rbrt@gmail.com>
2025-02-05 12:30:26 +0100

    Update main to 1.2.20dev version

--- web/b3desk/__init__.py
+++ web/b3desk/__init__.py
@@ -29,7 +29,7 @@ from b3desk.utils import is_rie
 from .utils import enum_converter
 from .utils import model_converter
 
-__version__ = "1.2.19dev"
+__version__ = "1.2.20dev"
 
 LANGUAGES = ["en", "fr"]
 
