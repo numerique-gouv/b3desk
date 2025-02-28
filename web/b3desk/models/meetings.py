77d5b18b2ca9f67bcdffa977ec0a055d8adbbc87
sebastien <sebastien@yaal.coop>
2025-02-28 16:28:19 +0100

    adds favorite in meeting class

--- web/b3desk/models/meetings.py
+++ web/b3desk/models/meetings.py
@@ -76,6 +76,7 @@ class Meeting(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now, nullable=False)
+    favorite = db.Column(db.Boolean, unique=False, default=False)
     user = db.relationship("User", back_populates="meetings")
     files = db.relationship("MeetingFiles", back_populates="meeting")
     externalFiles = db.relationship("MeetingFilesExternal", back_populates="meeting")
