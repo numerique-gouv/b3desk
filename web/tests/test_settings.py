c881aca72763d60a98ec4c37ec9a2eb94a1e5ce3
Éloi Rivard <eloi@yaal.coop>
2025-09-08 15:42:28 +0200

    fix: unit test due to last merge

--- web/tests/test_settings.py
+++ web/tests/test_settings.py
@@ -11,7 +11,9 @@ def test_list_of_strings_type(configuration):
     assert config_obj.OIDC_ATTENDEE_SCOPES == ["openid", "profile", "ect.scope.cv"]
 
 
-def test_enable_sip_true_with_and_without_private_key_home_page(client_app):
+def test_enable_sip_true_with_and_without_private_key_home_page(
+    client_app, visio_code_session
+):
     response = client_app.get("/", status=302)
     assert "/home" in response.location
     assert (
@@ -27,7 +29,7 @@ def test_enable_sip_true_with_and_without_private_key_home_page(client_app):
 
 
 def test_enable_sip_true_with_and_without_private_key_welcome_page(
-    client_app, authenticated_user
+    client_app, authenticated_user, visio_code_session
 ):
     response = client_app.get("/", status=302)
     assert "/welcome" in response.location
@@ -35,6 +37,7 @@ def test_enable_sip_true_with_and_without_private_key_welcome_page(
         "error",
         "La clé privée n'a pas été configurée dans les paramètres B3Desk pour sécuriser la connexion SIP",
     ) not in response.flashes
+
     client_app.app.config["PRIVATE_KEY"] = None
     response = client_app.get("/welcome", status=200)
     assert (
