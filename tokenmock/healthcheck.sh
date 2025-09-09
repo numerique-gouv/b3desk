69cef37d3f4a13242f3e8c1f7350744e5c0f76c9
Éloi Rivard <eloi@yaal.coop>
2025-09-09 13:28:33 +0200

    refactor: rewrote tokenmock in Python (#198)

---
+++ tokenmock/healthcheck.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+token=$(curl -X POST -H "X-API-KEY: $NC_LOGIN_API_KEY" -H "Content-Type: application/json" -d '{"username":"bbb-visio-user"}' http://localhost:5000/)
+
+if echo $token | grep -v -e "\"nctoken\":null" -e "Denied"
+then
+    exit 0
+else
+    exit 1
+fi
