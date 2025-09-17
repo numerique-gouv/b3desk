45be6e5147a0d0499d55d80bf3e2622876832ab3
Éloi Rivard <eloi@yaal.coop>
2025-09-17 15:24:13 +0200

    refactor: extract the visio code generation logic in its own file

---
+++ web/b3desk/static/js/meeting-form.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let buttons = document.getElementsByClassName("visio-code-button");
+    for (let button of buttons){
+        button.addEventListener("click", async (event) => {
+            let visioCodeInput = document.getElementById(event.target.dataset.fieldName)
+            const response = await fetch(window.visioCodeUrl, {
+                "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            const json = await response.json();
+            visioCodeInput.value = json.available_visio_code;
+        });
+    }
+});
