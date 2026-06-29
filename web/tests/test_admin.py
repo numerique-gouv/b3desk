4f9e3af2e2bebe85a5b2aaf435a16c108866494a
sebastien <sebastien@yaal.coop>
2026-06-29 14:00:07 +0200

    fix: fix error in tests

--- web/tests/test_admin.py
+++ web/tests/test_admin.py
@@ -103,7 +103,7 @@ def test_admin_can_read_user_infos_with_no_meeting(
     cli_runner.invoke(bp.cli, ["user-to-admin", "alice@domain.tld"])
     res = client_app.get("/admin/user/2", status=200)
     assert res.text.count("berenice@domain.tld") == 1
-    assert res.text.count('fr-badge fr-badge--error">Non</p>') == 2
+    assert res.text.count('fr-badge fr-badge--error">Non</p>') == 1
     assert "<em>Berenice Cooler</em> n'a pas créé de réunion." in res.text
 
 
